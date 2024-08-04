e02ac8866f183dd9ed2fd962081c4e5c94b479f8
babureddyg2308 <158761114+babureddyg2308@users.noreply.github.com>
2024-08-04 17:56:28 +0530

    backend

--- backend/Routes/ProductRouter.js
+++ backend/Routes/ProductRouter.js
@@ -1,22 +1,19 @@
+ 
+
 // const ensureAuthenticated = require('../Middlewares/Auth');
 
 // const router = require('express').Router();
 
 // router.get('/', ensureAuthenticated, (req, res) => {
-//     console.log('---- logged in user detail ---', req.user);
+//     console.log('---- Logged in user details ---', req.user);
 //     res.status(200).json([
-//         {
-//             name: "mobile",
-//             price: 10000
-//         },
-//         {
-//             name: "tv",
-//             price: 20000
-//         }
-//     ])
+//         { name: 'mobile', price: 10000 },
+//         { name: 'tv', price: 20000 }
+//     ]);
 // });
 
 // module.exports = router;
+
 
 const ensureAuthenticated = require('../Middlewares/Auth');
 
@@ -25,8 +22,16 @@ const router = require('express').Router();
 router.get('/', ensureAuthenticated, (req, res) => {
     console.log('---- Logged in user details ---', req.user);
     res.status(200).json([
-        { name: 'mobile', price: 10000 },
-        { name: 'tv', price: 20000 }
+        { 
+            image: 'https://www.ineedamobile.com/wp-content/uploads/2019/03/iphone-x-600x598.png',
+            name: 'mobile', 
+            price: 10000 
+        },
+        { 
+            image: 'https://cdn.pixabay.com/photo/2024/01/21/20/09/ai-generated-8523907_640.png',
+            name: 'tv', 
+            price: 20000 
+        }
     ]);
 });
 

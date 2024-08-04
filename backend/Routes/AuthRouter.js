8386f4145982b6936815b5c8c86a44819ba61f51
babureddyg2308 <158761114+babureddyg2308@users.noreply.github.com>
2024-08-04 23:03:54 +0530

    productes are added

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,14 +1,4 @@
-// const { signup, login } = require('../Controllers/AuthController');
-// const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
-
-// const router = require('express').Router();
-
-// router.post('/login', loginValidation, login);
-// router.post('/signup', signupValidation, signup);
-
-// module.exports = router;
-
-const { signup, login } = require('../Controllers/AuthController');
+ const { signup, login } = require('../Controllers/AuthController');
 const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
 
 const router = require('express').Router();
